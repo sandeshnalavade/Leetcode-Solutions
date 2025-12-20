5c7ae2cd5b8142e77912ffad7b74a908a4767acc
sandeshnalavade <sandeshnalavade2002@gmail.com>
2025-12-20 20:46:22 +0530

    Implement solution for 'Find the Difference' problem

---
+++ 389. Find the Difference.java
@@ -0,0 +1,7 @@
+lass Solution {
+  public char findTheDifference(String s, String t) {
+    final char sXors = (char) s.chars().reduce(0, (a, b) -> a ^ b);
+    final char tXors = (char) t.chars().reduce(0, (a, b) -> a ^ b);
+    return (char) (sXors ^ tXors);
+  }
+}
